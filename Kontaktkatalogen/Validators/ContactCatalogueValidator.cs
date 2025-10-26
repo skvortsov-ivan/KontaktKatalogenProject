a022132916e66b6d520116d9bbad5b4834f48d32
skvortsov-ivan <ivan.skvortsov@chasacademy.se>
2025-10-26 16:25:14 +0100

    Successfully implemented moq and xUnit tests.

--- Kontaktkatalogen/Validators/ContactCatalogueValidator.cs
+++ Kontaktkatalogen/Validators/ContactCatalogueValidator.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Kontaktkatalogen.Models;
 using Kontaktkatalogen.Repositories;
+using static Kontaktkatalogen.Models.InvalidExceptions;
 
 
 namespace Kontaktkatalogen.Validators
@@ -12,7 +13,7 @@ namespace Kontaktkatalogen.Validators
     public class ContactCatalogueValidator
     {
         //Exception if catalogue is empty
-        public void Validate(IContactCatalogue contactCatalogue)
+        public void AssertCatalogueNotEmpty(IContactCatalogue contactCatalogue)
         {
             if (contactCatalogue.Count == 0 || contactCatalogue == null)
                 throw new InvalidExceptions.EmptyCatalogueException("There are no available contacts in the catalogue.");
@@ -28,9 +29,11 @@ namespace Kontaktkatalogen.Validators
         //Exception if no contact has the user provided tag
         public List<Contact> ValidateTag(IContactCatalogue catalogue, string searchTag)
         {
-            var matchingContacts = catalogue.GetDictionary().Values.Where(c => c.Tags.Contains(searchTag)).ToList();
+            var matchingContacts = catalogue.GetDictionary().Values
+                .Where(c => c.Tags.Contains(searchTag))
+                .ToList();
 
-            if (catalogue.Count == 0)
+            if (!matchingContacts.Any())
                 throw new InvalidExceptions.MissingTagException("There are no contacts with this tag.");
 
             return matchingContacts;
